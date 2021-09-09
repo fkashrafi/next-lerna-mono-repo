fc0a37ec19178bae2128d020f6d916b59e62bc3b
fkashrafi <mfahadkhanashrafi@outlook.com>
2021-09-09 13:13:02 +0500

    first commit

---
+++ packages/users/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Router from 'next/router';
+import { AppProps } from "next/app";
+const App = ({ Component, pageProps }: AppProps) => {
+    return (
+        <Component {...pageProps} />
+    )
+}
+export default App;
